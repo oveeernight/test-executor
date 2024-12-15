508fce4b6dbf2fe95ef7be5deac3db0ae8eeea06
Roman Pozharskiy <rnpozharskiy@gmail.com>
2024-12-16 01:16:45 +0300

    add visitor

--- TestExecutor.Core/Models/IlTestExpr.cs
+++ TestExecutor.Core/Models/IlTestExpr.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Unicode;
+
 namespace TestExecutor.Core;
 
 
@@ -10,6 +13,7 @@ public class FieldRepr(TypeRepr type, string name);
 public abstract class IlTestStmt
 {
     public abstract StmtKind Kind { get; }
+    public abstract TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor);
 }
 
 public enum StmtKind
@@ -25,7 +29,7 @@ public enum StmtKind
 
 public abstract class IlTestExpr : IlTestStmt
 {
-    public abstract TypeRepr? Type { get; set; }
+    public abstract TypeRepr? TypeRepr { get; set; }
 }
 
 public abstract class IlTestConst<T> : IlTestExpr
@@ -33,114 +37,196 @@ public abstract class IlTestConst<T> : IlTestExpr
     public abstract T? Value { get; set; }
 }
 
-public class BoolConst(bool value, TypeRepr type) : IlTestConst<bool>
+public class BoolConst() : IlTestConst<bool>
 {
     public override StmtKind Kind { get; } = StmtKind.BOOL;
-    public override TypeRepr? Type { get; set; } = type;
-    public override bool Value { get; set; } = value;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitBool(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
+    public override bool Value { get; set; }
 }
 
-public class CharConst(char value, TypeRepr type) : IlTestConst<char>
+public class CharConst() : IlTestConst<char>
 {
     public override StmtKind Kind { get; } = StmtKind.CHAR;
-    public override TypeRepr? Type { get; set; } = type;
-    public override char Value { get; set; } = value;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitChar(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
+    public override char Value { get; set; }
 }
 
 public class Int8Const() : IlTestConst<sbyte>
 {
     public override StmtKind Kind { get; } = StmtKind.INT8;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitInt8(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override sbyte Value { get; set; }
 }
 
 public class Int16Const() : IlTestConst<short>
 {
     public override StmtKind Kind { get; } = StmtKind.INT16;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitInt16(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override short Value { get; set; }
 }
 
-public class Int32Const(int value, TypeRepr type) : IlTestConst<int>
+public class Int32Const() : IlTestConst<int>
 {
     public override StmtKind Kind { get; } = StmtKind.INT32;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitInt32(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override int Value { get; set; }
 }
 
-public class Int64Const(long value, TypeRepr type) : IlTestConst<long>
+public class Int64Const() : IlTestConst<long>
 {
     public override StmtKind Kind { get; } = StmtKind.INT64;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitInt64(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override long Value { get; set; }
 }
 
-public class UInt8Const(byte value, TypeRepr type) : IlTestConst<byte>
+public class UInt8Const() : IlTestConst<byte>
 {
     public override StmtKind Kind { get; } = StmtKind.UINT8;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitUInt8(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override byte Value { get; set; }
 }
 
-public class UInt16Const(ushort value, TypeRepr type) : IlTestConst<ushort>
+public class UInt16Const() : IlTestConst<ushort>
 {
     public override StmtKind Kind { get; } = StmtKind.UINT16;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitUInt16(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override ushort Value { get; set; }
 }
 
-public class UInt32Const(uint value, TypeRepr type) : IlTestConst<uint>
+public class UInt32Const() : IlTestConst<uint>
 {
     public override StmtKind Kind { get; } = StmtKind.UINT32;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitUInt32(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override uint Value { get; set; }
 }
 
-public class UInt64Const(ulong value, TypeRepr type) : IlTestConst<ulong>
+public class UInt64Const() : IlTestConst<ulong>
 {
     public override StmtKind Kind { get; } = StmtKind.UINT64;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitUInt64(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override ulong Value { get; set; }
 }
 
-public class FloatConst(float value, TypeRepr type) : IlTestConst<float>
+public class FloatConst() : IlTestConst<float>
 {
     public override StmtKind Kind { get; } = StmtKind.FLOAT;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitFloat(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override float Value { get; set; }
 }
 
-public class DoubleConst(double value, TypeRepr type) : IlTestConst<double>
+public class DoubleConst() : IlTestConst<double>
 {
     public override StmtKind Kind { get; } = StmtKind.DOUBLE;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitDouble(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override double Value { get; set; }
 }
 
 public class StringConst : IlTestConst<string>
 {
     public override StmtKind Kind { get; } = StmtKind.STRING;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitString(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
     public override string Value { get; set; }
 }
 
-public class NullConst(TypeRepr type) : IlTestConst<object>
+public class NullConst(): IlTestExpr
 {
     public override StmtKind Kind { get; } = StmtKind.INT8;
-    public override TypeRepr? Type { get; set; }
-    public override object? Value { get; set; } = null;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitNull(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
 }
 
-public class ArrayInstance(TypeRepr type, int size, int address) : IlTestExpr
+public class ArrayInstance() : IlTestExpr
 {
     public override StmtKind Kind { get; } = StmtKind.NEW_ARRAY;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitArrayInstance(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
+    public int Size { get; set; }
+    public int Address { get; set; }
 }
 
-public class ObjectInstance(TypeRepr type, int address) : IlTestExpr
+public class ObjectInstance() : IlTestExpr
 {
     public override StmtKind Kind { get; } = StmtKind.NEW_OBJ;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitObjectInstance(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
+    public int Address { get; set; }
 }
 
 public abstract class IlTestCall : IlTestExpr
@@ -152,8 +238,13 @@ public abstract class IlTestCall : IlTestExpr
 public class InstanceMethodCall : IlTestCall
 {
     public override StmtKind Kind { get; } = StmtKind.INSTANCE_CALL;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitInstanceMethodCall(this);
+    }
+
     public override MethodRepr Method { get; set; }
-    public override TypeRepr? Type { get; set; }
+    public override TypeRepr? TypeRepr { get; set; }
     public IlTestExpr Instance { get; set; }
     public override List<IlTestExpr> Args { get; } = [];
 }
@@ -161,16 +252,26 @@ public class InstanceMethodCall : IlTestCall
 public class StaticMethodCall : IlTestCall
 {
     public override StmtKind Kind { get; } = StmtKind.STATIC_CALL;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitStaticMethodCall(this);
+    }
+
     public override MethodRepr Method { get; set; }
-    public override TypeRepr? Type { get; set; }
+    public override TypeRepr? TypeRepr { get; set; }
     public override List<IlTestExpr> Args { get; } = [];
 }
 
 public class ConstructorCall : IlTestCall
 {
     public override StmtKind Kind { get; } = StmtKind.CONSTRUCTOR_CALL;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitConstructorCall(this);
+    }
+
     public override MethodRepr Method { get; set; }
-    public override TypeRepr? Type { get; set; }
+    public override TypeRepr? TypeRepr { get; set; }
     public override List<IlTestExpr> Args { get; }
 }
 
@@ -182,6 +283,11 @@ public abstract class ArrangeStmt : IlTestStmt
 public class SetArrayIndex : ArrangeStmt
 {
     public override StmtKind Kind { get; } = StmtKind.SET_ARRAY_INDEX;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitSetArrayIndex(this);
+    }
+
     public override IlTestExpr Instance { get; set; }
     public int Index { get; set; }
     public IlTestExpr Value { get; set; }
@@ -190,6 +296,11 @@ public class SetArrayIndex : ArrangeStmt
 public class SetObjectField : ArrangeStmt
 {
     public override StmtKind Kind { get; } = StmtKind.SET_OBJ_FIELD;
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitSetObjectField(this);
+    }
+
     public override IlTestExpr Instance { get; set; }
     public FieldRepr Field { get; set; }
     public IlTestExpr Value { get; set; }
@@ -198,13 +309,23 @@ public class SetObjectField : ArrangeStmt
 public class IlTypeInstance : IlTestExpr
 {
     public override StmtKind Kind { get; } = StmtKind.TYPE_INSTANCE;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitIlTypeInstance(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
 }
 
 public class CyclicReference : IlTestExpr
 {
     public override StmtKind Kind { get; } = StmtKind.CYCLIC_REFERENCE;
-    public override TypeRepr? Type { get; set; }
+    public override TResult Accept<TResult>(ITestStmtVisitor<TResult> visitor)
+    {
+        return visitor.VisitCyclicRef(this);
+    }
+
+    public override TypeRepr? TypeRepr { get; set; }
 }
 
 
---
+++ TestExecutor.Core/Models/IlTestStmtVisitor.cs
@@ -0,0 +1,29 @@
+namespace TestExecutor.Core;
+
+public interface ITestStmtVisitor<out TResult>
+{
+    TResult VisitBool(BoolConst stmt);
+    TResult VisitChar(CharConst stmt);
+    TResult VisitInt8(Int8Const stmt);
+    TResult VisitInt16(Int16Const stmt);
+    TResult VisitInt32(Int32Const stmt);
+    TResult VisitInt64(Int64Const stmt);
+    TResult VisitUInt8(UInt8Const stmt);
+    TResult VisitUInt16(UInt16Const stmt);
+    TResult VisitUInt32(UInt32Const stmt);
+    TResult VisitUInt64(UInt64Const stmt);
+    TResult VisitFloat(FloatConst stmt);
+    TResult VisitDouble(DoubleConst stmt);
+    TResult VisitString(StringConst stmt);
+    TResult VisitNull(NullConst stmt);
+    TResult VisitInstanceMethodCall(InstanceMethodCall stmt);
+    TResult VisitStaticMethodCall(StaticMethodCall stmt);
+    TResult VisitConstructorCall(ConstructorCall stmt);
+    TResult VisitArrayInstance(ArrayInstance stmt);
+    TResult VisitObjectInstance(ObjectInstance stmt);
+    TResult VisitSetArrayIndex(SetArrayIndex stmt);
+    TResult VisitSetObjectField(SetObjectField stmt);
+    TResult VisitIlTypeInstance(IlTypeInstance stmt);
+    TResult VisitCyclicRef(CyclicReference stmt);
+
+}
